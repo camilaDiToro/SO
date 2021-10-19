e6ba4bf1959cddb5f841e4f307ae256a044c312c
ichayer <ichayer@itba.edu.ar>
2021-10-19 18:12:39 -0300

    fix merge problems

--- Userland/SampleCodeModule/sampleCodeModule.c
+++ Userland/SampleCodeModule/sampleCodeModule.c
@@ -12,17 +12,10 @@ int main() {
 
 	sprint(1,"Welcome to Userland");
 
-<<<<<<< HEAD
-	// while(1){
-	// 	put_char(2,'>');
-	// 	wait_command();
-	// }
-=======
 	while(1){
 		put_char(2,'>');
 		wait_command();
 	}
->>>>>>> main
 
 	//Test if BSS is properly set up
 	if (var1 == 0 && var2 == 0)
