146ba308312e1a8a4e2a52112f798d6d9d0faad4
ThomasMiz <32400648+ThomasMiz@users.noreply.github.com>
2022-05-15 21:50:22 -0300

    Fixed compilation issue on userland

--- Userland/SampleCodeModule/_loader.c
+++ Userland/SampleCodeModule/_loader.c
@@ -12,7 +12,7 @@ void* memset(void* destiny, int32_t c, size_t length);
 int _start(int argc, char * argv[]) {
     // Clean BSS
     memset(&bss, 0, &endOfBinary - &bss);
-    return main(argc, argv);
+    return main(0, 0);
 }
 
 void* memset(void* destiation, int32_t c, size_t length) {
