0d38b6194923e7b506b05461b2777c6dcbb88a85
camilaDiToro <camiladitoro@gmail.com>
2022-05-18 17:55:37 -0300

    Fix warnings

--- Kernel/semaphores.c
+++ Kernel/semaphores.c
@@ -5,11 +5,10 @@
 // Now just are being counted the number of processes, but this could lead to an inconsistency if,
 // for example, a process tries to close the same semaphore twice
 typedef struct {
-    TSem semId; 
     uint8_t value;
     TLock lock;
     uint8_t linkedProcesses; 
-    char* name;
+    const char* name;
     TWaitQueue waitingProcesses;  //Tracks the processes waiting for a post in this semaphore
 } TSemaphore;
 
@@ -48,6 +47,7 @@ static int adquireSem(TSem sem){
 
     _spin_lock(&(semaphores[sem]->lock));
     generalLock = 0;
+    return SEM_SUCCES;
 }
 
 int sem_printDebug() {
@@ -64,8 +64,8 @@ int sem_printDebug() {
             scr_print(semaphores[sem]->name);
             scr_printChar('\n');
         }
-
     }
+    return SEM_SUCCES;
 }
 
 int sem_init() {
@@ -80,16 +80,16 @@ TSem sem_open(const char * name, uint8_t initialValue) {
 
     _spin_lock(&generalLock);
 
-    TSemaphore * sem = rnm_getResource(namer, name);
+    TSem sem = (TSem)(int64_t)rnm_getResource(namer, name);
 
-    if(sem!=NULL){
-        sem->linkedProcesses+=1;
+    if(sem!=0){
+        semaphores[sem]->linkedProcesses+=1;
         generalLock = 0;
-        return sem->semId;
+        return sem;
     }
     
     int i;
-    for( i=0 ; i < MAX_SEMAPHORES && semaphores[i]; ++i );
+    for( i=1 ; i < MAX_SEMAPHORES && semaphores[i]; ++i );
 
     if(i == MAX_SEMAPHORES) {
         generalLock = 0;
@@ -102,7 +102,6 @@ TSem sem_open(const char * name, uint8_t initialValue) {
         return SEM_FAILED;
     }
     semaphores[i]->value = initialValue;
-    semaphores[i]->semId = i;
     semaphores[i]->lock = 0;
     semaphores[i]->linkedProcesses = 1;
     semaphores[i]->waitingProcesses = wq_new();
@@ -113,7 +112,7 @@ TSem sem_open(const char * name, uint8_t initialValue) {
         return SEM_FAILED;
     }
 
-    if(rnm_nameResource(namer, semaphores[i], name, &(semaphores[i]->name))!=0){
+    if(rnm_nameResource(namer,(void *)(int64_t)i, name, &(semaphores[i]->name))!=0){
         //check what should be freed
         generalLock = 0;
         return SEM_FAILED;
